36b76e01c715868c231e0e1be2ffa0ce3b0ad810
sbmart <sbmart73@gmail.com>
2019-10-07 13:12:35 +0300

    initial dirty class stateful component

--- src/App.js
+++ src/App.js
@@ -12,7 +12,49 @@ class ListComponent extends React.Component {
   render() {
     return (
       <div>
-        hello
+        <h1>The last clicked button is {this.state.lastClickedButton}</h1>
+        <ul>
+          <li>
+            <button
+              onClick={() => {
+                this.setState({ lastClickedButton: 'Create '});
+                this.props.createSomething();
+              }}
+            className="my-button">
+            Create
+            </button>
+          </li>
+          <li>
+            <button
+              onClick={() => {
+                this.setState({ lastClickedButton: 'Read' });
+                this.props.readSomething();
+              }}
+            className="my-button">
+            Read
+            </button>
+          </li>
+          <li>
+            <button 
+            onClick={() => {
+              this.setState({ lastClickedButton: 'Update'});
+              this.props.updateSomething();
+            }}
+            className="my-button">
+              Updata
+            </button>
+          </li>
+          <li>
+            <button 
+              onClick={() => {
+                this.setState({ lastClickedButton: 'Destroy'});
+                this.props.destroySomething();
+              }}
+            className="my-button">
+              Destroy
+            </button>
+          </li>
+        </ul>
       </div>
     )
   }
